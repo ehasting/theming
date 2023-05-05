07f8b9494b8774359cba408585a6677b53619d65
Egil Hasting <eh@synergysky.com>
2023-05-05 11:07:34 +0200

    ohmyzsh + zsh in setup

--- setup.sh
+++ setup.sh
@@ -6,3 +6,8 @@ ln $(pwd)/Xresources ~/.Xresources || true
 echo "Update database"
 xrdb -merge ~/.Xresources
 
+sudo dnf install zsh curl-y
+chsh -s /usr/bin/zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
