42f26f3a16a6879acb08abd6db4b5b504d0ff11f
Egil Hasting <eh@synergysky.com>
2023-05-05 11:08:47 +0200

    urxvt

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ ln $(pwd)/Xresources ~/.Xresources || true
 echo "Update database"
 xrdb -merge ~/.Xresources
 
-sudo dnf install zsh curl-y
+sudo dnf install zsh curl rxvt-unicode -y
 chsh -s /usr/bin/zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
